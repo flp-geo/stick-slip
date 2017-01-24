80406333a714d31519cce966a657fbc5bd41550e
flpetersen <flpetersen@geomar.de>
2017-01-24 17:29:07 +0100

    moving average slope implementation

--- stickslip.py
+++ stickslip.py
@@ -14,18 +14,33 @@ import scipy
 
 ifile = open('1_1.ascii', 'r')
 
-dist_tmp = []
-shear_stress_tmp = [] 
+dist = []
+shear_stress = [] 
 all_data = ifile
 
 
 for i,line in enumerate(all_data):
 	line = line.rstrip()
 	raw_data = line.split('\t')
-	dist_tmp.append(raw_data[0])
-	shear_stress_tmp.append(raw_data[1])
+	dist.append(float(raw_data[0]))
+	shear_stress.append(float(raw_data[1]))
 
-plt.plot(dist_tmp,shear_stress_tmp)
-plt.show()
+plt.plot(shear_stress)
 
 ifile.close()
+
+for i in dist
+	dY=i[]
+
+
+dY = (np.roll(shear_stress, -1, axis=0) - shear_stress)[:-1]
+dX = (np.roll(dist, -1, axis=0) - dist)[:-1]
+
+slopes = dY/dX
+
+for x, y  in enumerate(slopes):
+	if y == 0:
+		print dist[x]
+
+plt.plot(slopes)
+plt.show()
